af1d8a3bf279ba807b61b3a9f9466438a56b6d06
Madeline Ng <madelineng@Madelines-MacBook-Pro-3.local>
2019-03-07 10:04:23 -0800

    fiddle

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,57 @@
 /*
  * GET home page.
  */
+var labs = [
+	{
+		"id": "lab-1",
+		"title": "Source Control",
+		"date": "January 10",
+		"image": "octocat.jpg"
+	},
+	{
+		"id": "lab-2",
+		"title": "Styling",
+		"date": "January 17",
+		"image": "css.png"
+	},
+	{
+		"id": "lab-3",
+		"title": "Interactive Web pages",
+		"date": "January 24",
+		"image": "js.png"
+	},
+	{
+		"id": "lab-4",
+		"title": "Servers",
+		"date": "January 31",
+		"image": "nodejs.png"
+	},
+	{
+		"id": "lab-5",
+		"title": "Putting it together",
+		"date": "February 7",
+		"image": "html-css-js.jpg"
+	},
+	{
+		"id": "lab-6",
+		"title": "FB Login & AJAX",
+		"date": "February 14",
+		"image": "ajax.jpg"
+	},
+	{
+		"id": "lab-7",
+		"title": "Analytics and A/B Testing",
+		"date": "February 21",
+		"image": "analytics.png"
+	},
+	{
+		"id": "lab-8",
+		"title": "APIs, Gesture, and AJAX",
+		"date": "February 28",
+		"image": "api.png"
+	}
+]
+
 var labs = require('../labs.json');
 
 exports.view = function(req, res){
@@ -14,6 +65,8 @@ exports.view = function(req, res){
 
   	labs.push(todaysLab);
   }
+
+console.log(labs);
 
   res.render('index', {
 	'labs': labs
